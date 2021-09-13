e395327bb3bfda9ef0d66a2bbeff4dd1a4485c84
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-09-13 16:35:31 +0530

    Initial Commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout';
+function Home() {
+  return (
+    <div>
+    <Layout>
+    <h1 className='text-center'>Welcome to Admin Dashboard</h1>
+    </Layout>
+    </div>
+  )
+}
+
+export default Home
