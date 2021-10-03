ae1d4d7c065976fd906dc65b8aadbf0bcf7a40ae
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-10-03 16:15:02 +0530

    formatted file

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,80 +1,105 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import Layout from '../../components/Layout';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 import Input from '../../components/UI/Input';
-import { useSelector, useDispatch} from 'react-redux';
-import {Redirect} from 'react-router-dom';
-import {signup} from '../../actions'
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { signup } from '../../actions';
 
 function Signup() {
-
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // const [error, setError] = useState('');
-  const user = useSelector(state => state.user);
+  const [msg, setMsg] = useState('');
+  const user = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
 
   const userSignup = (e) => {
     e.preventDefault();
-    const user = {firstName, lastName, email, password};
-    dispatch(signup(user));
+    const user = { firstName, lastName, email, password };
+    const msg = dispatch(signup(user))
+      .then(() => {
+        setMsg('You have successfully signed up. Now you can Sign In');
+        emptyInputFields();
+      })
+      .catch((err) => {
+        setMsg(err.message);
+      });
+    console.log(msg);
+  };
+
+  const emptyInputFields = () => {
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPassword('');
+  }
+  const auth = useSelector((state) => state.auth);
+
+  if (auth.authenticate) {
+    return <Redirect to={'/'} />;
   }
 
-  const auth = useSelector(state => state.auth);
-
-  if(auth.authenticate){
-    return <Redirect to={'/'}/>;
-  }
-
-  if(user.loading){
-    return (<p>Loading...</p>);
+  if (user.loading) {
+    return <p>Loading...</p>;
   }
 
   return (
     <div>
-     <Layout>
+      <Layout>
         <Container>
-        {user.message}
-          <Row style={{marginTop: '50px'}}>
-            <Col md={{span:6, offset: 3}}>
+          <Row style={{ marginTop: '50px' }}>
+            <Col style={{ textAlign: 'center' }}>
+              <p style={{ paddingTop: '20px' , color: 'red'}}>{msg}</p>
+            </Col>
+          </Row>
+          <Row style={{ marginTop: '50px' }}>
+            <Col md={{ span: 6, offset: 3 }}>
               <Form onSubmit={userSignup}>
-              <Row>
-                <Col md={6}>
+                <Row>
+                  <Col md={6}>
+                    <Input
+                      label="First Name"
+                      placeholder="First Name"
+                      value={firstName}
+                      type="text"
+                      onChange={(e) => {
+                        setFirstName(e.target.value);
+                      }}
+                    />
+                  </Col>
+                  <Col md={6}>
+                    <Input
+                      label="Last Name"
+                      placeholder="Last Name"
+                      value={lastName}
+                      type="text"
+                      onChange={(e) => {
+                        setLastName(e.target.value);
+                      }}
+                    />
+                  </Col>
+                </Row>
                 <Input
-                  label="First Name"
-                  placeholder="First Name"
-                  value={firstName}
-                  type="text"
-                  onChange={(e)=>{setFirstName(e.target.value)}}
-                />
-                </Col>
-                <Col md={6}>
-                <Input
-                  label="Last Name"
-                  placeholder="Last Name"
-                  value={lastName}
-                  type="text"
-                  onChange={(e)=>{setLastName(e.target.value)}}
-                />
-                </Col>
-              </Row>
-              <Input
                   label="Email"
                   placeholder="Email"
                   value={email}
                   type="email"
-                  onChange={(e)=>{setEmail(e.target.value)}}
-              />
-              <Input
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+                <Input
                   label="Password"
                   placeholder="Password"
                   value={password}
                   type="password"
-                  onChange={(e)=>{setPassword(e.target.value)}}
-              />
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
@@ -84,7 +109,7 @@ function Signup() {
         </Container>
       </Layout>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
